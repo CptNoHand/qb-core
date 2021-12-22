b8ee3630e80716b9b89d6435949837d8dbc6c6ee
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-22 20:17:46 +0100

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -2008,12 +2008,13 @@ QBShared.Jobs = {
 	['catcafe'] = {
 		label = 'Cat Cafe',
 		defaultDuty = true,
+		offDutyPay = false,
 		grades = {
-            ['0'] = { name = 'Recruit', payment = 50 },
-			['1'] = { name = 'Novice', payment = 75 },
-			['2'] = { name = 'Experienced', payment = 100 },
-			['3'] = { name = 'Advanced', payment = 125 },
-			['4'] = { name = 'Manager', isboss = true, payment = 150 },
+            ['0'] = { name = 'Recruit', payment = 0 },
+			['1'] = { name = 'Novice', payment = 0 },
+			['2'] = { name = 'Experienced', payment = 0 },
+			['3'] = { name = 'Advanced', payment = 0 },
+			['4'] = { name = 'Manager', isboss = true, payment = 0 },
         },
 	},
 	['judge'] = {
